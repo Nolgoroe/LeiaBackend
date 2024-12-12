4ca8597eb5593d2fe6bc89fefc5bdf5c9d442166
Lital Natan <deboxed360@gmail.com>
2024-12-12 16:34:03 +0200

    Added a 3-player glicko rating calc test

--- LeiaMain/ServicesTests/PostTournamentServiceTests.cs
+++ LeiaMain/ServicesTests/PostTournamentServiceTests.cs
@@ -3,9 +3,9 @@ using Glicko2;
 using Services;
 using DataObjects;
 
-namespace MyNamespace
+namespace ServiceTests
 {
-    public class UnitTest1
+    public class PostTournamnetServiceTests
     {
         GlickoPlayer player1 = new GlickoPlayer(ratingDeviation: 200);
         GlickoPlayer player2 = new GlickoPlayer(1400, 30);
@@ -13,7 +13,7 @@ namespace MyNamespace
         GlickoPlayer player4 = new GlickoPlayer(1700, 300);
 
         [Fact]
-        public void TestCalculatePlayerRating()
+        public void TestCalculatePlayerRating2Player()
         {
             var startRating = 1500;
             var maxRatingChange = 100; // amount of maximum rating drift per each player
@@ -53,5 +53,61 @@ namespace MyNamespace
             Assert.True(player2NewRating < startRating + maxRatingChange);
         }
 
+
+        [Fact]
+        public void TestCalculatePlayerRating3Player()
+        {
+            var startRating = 1500;
+            var maxRatingChange = 100; // amount of maximum rating drift per each player
+            var tournamentSession = new TournamentSession();
+            var player1 = new Player()
+            {
+                PlayerId = Guid.NewGuid(),
+                Rating = startRating,
+            };
+            var playerSession1 = new PlayerTournamentSession()
+            {
+                PlayerId = player1.PlayerId,
+                PlayerScore = 100,
+            };
+            var player2 = new Player()
+            {
+                PlayerId = Guid.NewGuid(),
+                Rating = startRating,
+            };
+            var playerSession2 = new PlayerTournamentSession()
+            {
+                PlayerId = player2.PlayerId,
+                PlayerScore = 180,
+            };
+            var player3 = new Player()
+            {
+                PlayerId = Guid.NewGuid(),
+                Rating = startRating,
+            };
+            var playerSession3 = new PlayerTournamentSession()
+            {
+                PlayerId = player3.PlayerId,
+                PlayerScore = 200,
+            };
+            Assert.NotEqual(player1.PlayerId, player2.PlayerId);
+            tournamentSession.PlayerTournamentSessions.Add(playerSession1);
+            tournamentSession.PlayerTournamentSessions.Add(playerSession2);
+            tournamentSession.PlayerTournamentSessions.Add(playerSession3);
+            tournamentSession.Players.Add(player3);
+            tournamentSession.Players.Add(player2);
+            tournamentSession.Players.Add(player1);
+            var ratingResults = PostTournamentService.CalculatePlayersRatingFromTournament(tournamentSession);
+            var player1NewRating = ratingResults[player1.PlayerId];
+            var player2NewRating = ratingResults[player2.PlayerId];
+            var player3NewRating = ratingResults[player3.PlayerId];
+            Assert.True(player1NewRating < startRating);
+            Assert.True(player2NewRating > startRating);
+            Assert.True(player1NewRating < player2NewRating, "Loser got greater rating than winner");
+            Assert.True(player2NewRating < player3NewRating, "Loser got greater rating than winner");
+            Assert.True(player1NewRating > startRating - maxRatingChange);
+            Assert.True(player2NewRating < startRating + maxRatingChange);
+            Assert.True(player3NewRating < startRating + maxRatingChange);
+        }
     }
 }
