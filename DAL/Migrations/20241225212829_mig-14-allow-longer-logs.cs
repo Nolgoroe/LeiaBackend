0c6e59fe8dadbe3d6fc2fa240825c5cb30bffafe
Lital Natan <lital@springames.com>
2024-12-25 23:35:40 +0200

    Added missing migration

---
+++ DAL/Migrations/20241225212829_mig-14-allow-longer-logs.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig14allowlongerlogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Log",
+                table: "BackendLogs",
+                type: "nvarchar(max)",
+                maxLength: 8196,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Log",
+                table: "BackendLogs",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 8196);
+        }
+    }
+}
