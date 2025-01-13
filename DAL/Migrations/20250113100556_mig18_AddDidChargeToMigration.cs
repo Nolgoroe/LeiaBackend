034730679157c6ad6be1117f8a5746068c123417
Alon Buyum <alon@leia.games>
2025-01-13 12:34:31 +0200

    add DidCharge in PlayerActiveTournament to a Migration. To keep consistency in the database

---
+++ DAL/Migrations/20250113100556_mig18_AddDidChargeToMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig18_AddDidChargeToMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DidCharge",
+                table: "PlayerActiveTournaments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DidCharge",
+                table: "PlayerActiveTournaments");
+        }
+    }
+}
