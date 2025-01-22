50b78f991a4b6fa9544b26341e1031f11a5f4e86
Lital Natan <lital@springames.com>
2025-01-22 20:27:29 +0200

    Calculate glicko rating according to the specific tournament type of the player

--- DataObjects/PlayerTournamentSession.cs
+++ DataObjects/PlayerTournamentSession.cs
@@ -8,7 +8,8 @@ namespace DataObjects
     /// <summary>
     ///MtM table with payload(=extra data about each link. In this case, what score each player had in the tournament)
     /// </summary>
-    public class PlayerTournamentSession // the name of the  class should be a combination of the names of both the classes that are being combined. Where the first name should be name of the class who's Id prop appears first. e.g in this combination, the Player is first in the props and thus first in  the name 
+    // the name of the  class should be a combination of the names of both the classes that are being combined. Where the first name should be name of the class who's Id prop appears first. e.g in this combination, the Player is first in the props and thus first in  the name 
+    public class PlayerTournamentSession : IComparable<PlayerTournamentSession>
     {
         public Guid PlayerId { get; set; }
         [JsonIgnore]
@@ -25,5 +26,16 @@ namespace DataObjects
         public int TournamentTypeId { get; set; }
         [ForeignKey(nameof(TournamentTypeId))]
         public TournamentType TournamentType { get; set; }
+
+        /// <summary>
+        /// Compare by player score
+        /// </summary>
+        public int CompareTo(PlayerTournamentSession? other)
+        {
+            if (other == null) return 1;
+            if (other.PlayerScore == null) return -1;
+            if (PlayerScore == null) return -1;
+            return PlayerScore.Value - other.PlayerScore.Value;
+        }
     }
 }
