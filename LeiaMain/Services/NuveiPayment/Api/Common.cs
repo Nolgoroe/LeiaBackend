56f1aacf9da6946c0811ddb88fb3fd96492997f2
Itai <itai@satati.co>
2025-02-18 10:36:57 +0200

    Create additional request files

--- LeiaMain/Services/NuveiPayment/Api/Common.cs
+++ LeiaMain/Services/NuveiPayment/Api/Common.cs
@@ -1,23 +1,69 @@
 ﻿namespace Services.NuveiPayment.Api
 {
-    public class DeviceDetails
-    {
-        public string ipAddress;
-    }
 
-    public class PaymentOptionCard
-    {
-        public string cardNumber;
-        public string cardHolderName;
-        public string expirationMonth;
-        public string expirationYear;
-        public string CVV;
-        public object threeD; // Need to research this further 
-    }
+	public class PaymentOptionCard
+	{
+		public required string cardHolderName;
+		public required string cardNumber;
+		public required string expirationMonth;
+		public required string expirationYear;
+		public required string CVV;
+	}
 
-    public class PaymentOptionRoot
-    {
-        public PaymentOptionCard card;
-    }
+	public class PaymentOptionToken
+	{
+		public required string userTokenId;
+	}
 
+	public class PaymentOptionRoot
+	{
+		public PaymentOptionCard? card;
+		public string? userPaymentOptionId;
+	}
+
+	public class DeviceDetails
+	{
+		public required string ipAddress;
+		public string? deviceType;
+	}
+
+	public class BillingAddressDetails
+	{
+		public required string country;
+		public required string email;
+		public required string firstName;
+		public required string lastName;
+	}
+
+	public class DynamicDescriptor
+	{
+		public required string merchantName;
+		public required string merchantPhone;
+	}
+
+	public class UserPaymentOption
+	{
+		public required string userPaymentOptionId;
+	}
+
+	public class PaymentOptionResponseCard
+	{
+		public required string ccCardNumber;
+		public required string bin;
+		public required string ccExpMonth;
+		public required string ccExpYear;
+		public required string last4Digits;
+	}
+
+	public class PaymentOptionResponseRoot
+	{
+		public PaymentOptionResponseCard? card;
+		public string? userPaymentOptionId;
+	}
+
+	public class FraudDetailsResponse
+	{
+		public required string finalDecision;
+		public required string score;
+	}
 }
